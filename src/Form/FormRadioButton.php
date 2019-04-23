468652f2b9006082f2e5067c0154f19d50125513
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2019-04-23 19:02:31 +0200

    fix custom radio value

--- src/Form/FormRadioButton.php
+++ src/Form/FormRadioButton.php
@@ -12,8 +12,6 @@ declare(strict_types=1);
 
 namespace InspiredMinds\ContaoExtendedFormFieldsBundle\Form;
 
-use Contao\Input;
-
 class FormRadioButton extends \Contao\FormRadioButton
 {
     public function parse($arrAttributes = null)
@@ -27,22 +25,23 @@ class FormRadioButton extends \Contao\FormRadioButton
 
     public function validate(): void
     {
-        if ($this->addCustomOption) {
-            $customValue = $this->getPost($this->strName.'_custom'.$this->id);
+        parent::validate();
+
+        if ($this->addCustomOption && $this->getCustomRadioValue() === $this->getPost($this->strName)) {
+            $customValue = $this->getPost($this->getCustomTextName());
 
             if (!empty($customValue)) {
-                Input::setPost($this->strName, $customValue);
+                $this->varValue = $customValue;
+            }
+            else {
+                $this->addError($GLOBALS['TL_LANG']['ERR']['invalid']);
             }
         }
-
-        parent::validate();
     }
 
     protected function isValidOption($varInput)
     {
-        $customValue = $this->getPost($this->strName.'_custom'.$this->id);
-
-        if (!empty($customValue) && $customValue === $varInput) {
+        if ($varInput === $this->getCustomRadioValue()) {
             return true;
         }
 
@@ -53,23 +52,30 @@ class FormRadioButton extends \Contao\FormRadioButton
     {
         if ($this->addCustomOption) {
             $this->arrOptions[] = [
-                'value' => 'custom'.$this->id,
-                'label' => 'custom'.$this->id,
+                'value' => $this->getCustomRadioValue(),
+                'label' => $GLOBALS['TL_LANG']['MSC']['formFieldCustomLabel'],
             ];
         }
 
         $options = parent::getOptions();
 
         foreach ($options as &$option) {
-            if ($option['value'] === 'custom'.$this->id && $option['label'] === 'custom'.$this->id) {
+            if ($option['value'] === $this->getCustomRadioValue()) {
                 $option['type'] = 'custom';
-                $option['value'] = $this->isValidOption($this->varValue) ? $options['value'] : $this->varValue;
                 $option['checked'] = $this->isValidOption($this->varValue) ? '' : ' checked';
-                $option['custom_name'] = $this->strName.'_custom'.$this->id;
-                $option['custom_value'] = $this->varValue;
+                $option['custom_name'] = $this->getCustomTextName();
+                $option['custom_value'] = $this->isValidOption($this->varValue) ? '' : $this->varValue;
             }
         }
 
         return $options;
+    }
+
+    protected function getCustomRadioValue() {
+        return 'custom'.$this->id;
+    }
+
+    protected function getCustomTextName() {
+        return $this->strName.'_custom'.$this->id;
     }
 }
--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -13,3 +13,5 @@ declare(strict_types=1);
 $GLOBALS['TL_LANG']['ERR']['formFieldMinOptions'] = 'You have to select at least %s options.';
 $GLOBALS['TL_LANG']['ERR']['formFieldMaxOptions'] = 'You can only select a maximum of %s options.';
 $GLOBALS['TL_LANG']['ERR']['formFieldBlacklistedWords'] = 'The word "%s" is not allowed.';
+
+$GLOBALS['TL_LANG']['MSC']['formFieldCustomLabel'] = 'Custom value';
